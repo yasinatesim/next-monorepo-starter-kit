6b08ffc8ae4f99214e8908f539a5735f65414b5e
Yasin ATEŞ <yasinatesim@gmail.com>
2021-09-20 23:24:56 +0300

    chore: project init

---
+++ packages/shared/src/components/atoms/Button/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export const Button= ({
+    className,
+    id,
+    onClick,
+    buttonType,
+    ...props
+}) => {
+    return (
+        <>
+            <button
+                >akldkjds</button>
+        </>
+    )
+}
+
