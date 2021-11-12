625f56c2624e97fc9db86fc368b9df6380e92bf5
Yasin ATEŞ <yasinatesim@gmail.com>
2021-11-12 21:05:05 +0300

    feat: remove unused codes

--- packages/desktop/src/universal/containers/sidebar/index.js
+++ packages/desktop/src/universal/containers/sidebar/index.js
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-
 import { LogoWhite, Plus, Heart } from '@spotify-clone/shared/src/universal/components/icon';
 
 import Menu from './components/menu';
@@ -7,15 +5,9 @@ import Menu from './components/menu';
 import s from './index.module.scss';
 
 function Sidebar() {
-  const [width, setWidth] = useState(false);
-
   return (
-    <aside className={s.sidebar} style={{ width: `${width}px` }}>
-      <div className={s.resizer}>
-        <label>
-          <input type="range" min="120" max="384" step="10" value={width} onChange={(e) => setWidth(e.target.value)} />
-        </label>
-      </div>
+    <aside className={s.sidebar}>
+      <label className={s.resizer} />
       <a href="#" className={s.logo}>
         <LogoWhite height={40} />
       </a>
