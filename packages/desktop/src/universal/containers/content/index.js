11edc3715b76ce3044eb81ced0054f8977f744ae
Yasin ATEŞ <yasinatesim@gmail.com>
2021-12-04 20:35:07 +0300

    feat: update styles for content area

--- packages/desktop/src/universal/containers/content/index.js
+++ packages/desktop/src/universal/containers/content/index.js
@@ -6,10 +6,11 @@ import s from './index.module.scss';
 
 function Content({ children }) {
   return (
-    <main className={s.content}>
+    <main className={s.wrapper}>
       <Header />
 
-      {children}
+      {/* https://api.spotify.com/v1/views/desktop-home?timestamp=2021-12-01T23%3A04%3A20.381Z&platform=web&content_limit=10&limit=20&types=album%2Cplaylist%2Cartist%2Cshow%2Cstation%2Cepisode&image_style=gradient_overlay&include_external=audio&country=TR&locale=en&market=TR */}
+      <div className={s.content}>{children}</div>
     </main>
   );
 }
